d4e3726ee299ffd314f2bb00740251101a958acd
rickkert1999 <rickkert1999@gmail.com>
2022-04-14 11:44:24 +0200

    Use frame counter instead of time counter
    This will display the text more precisely

--- Overlay test/javascript/javascript.js
+++ Overlay test/javascript/javascript.js
@@ -12,71 +12,71 @@ fetch("reservations.json")
     //Id of the reservation
     let reservationId = 0;
 
-    //Count time that video is playing
-    function timedCount() {
-      counter++;
-      console.log(counter);
+    var currentFrame = document.getElementById("video");
+    var video = VideoFrame({
+      id: "video",
+      frameRate: 60,
+      callback: function (frame) {
+        currentFrame.innerHTML = frame;
+        console.log(frame);
 
-      timeout = setTimeout(timedCount, 10);
+        if (frame > 400 && frame < 600) {
+          textOverlay.innerHTML =
+            "<p id=insertedText>We are prepairing everything <br> for your visit!</p>";
+        } else if (frame > 880 && frame < 1108) {
+          //for each guest write down the name
+          for (
+            var i = 0;
+            i < dataGuest.reservations[reservationId].guests.length;
+            i++
+          ) {
+            var guest = dataGuest.reservations[reservationId].guests[i];
+            var guestName = guest.firstName + " " + guest.lastName;
 
-      if (counter > 400 && counter < 600) {
-        textOverlay.innerHTML =
-          "<p id=insertedText>We are prepairing everything <br> for your visit!</p>";
-      } else if (counter > 880 && counter < 1108) {
-        //for each guest write down the name
-        for (
-          var i = 0;
-          i < dataGuest.reservations[reservationId].guests.length;
-          i++
-        ) {
-          var guest = dataGuest.reservations[reservationId].guests[i];
-          var guestName = guest.firstName + " " + guest.lastName;
+            if (frame === 881) {
+              const divName = document.createElement("div");
+              divName.setAttribute("id", "guestInfo" + i);
+              divName.setAttribute("class", "guestInfo");
+              divName.innerHTML = "<p id=name" + i + ">" + guestName + "</p>";
 
-          if (counter === 881) {
-            const divName = document.createElement("div");
-            divName.setAttribute("id", "guestInfo" + i);
-            divName.setAttribute("class", "guestInfo");
-            divName.innerHTML = "<p id=name" + i + ">" + guestName + "</p>";
-
-            textOverlay.appendChild(divName);
-          }
-
-          console.log(guestName);
-          //for each guest write down the preferance
-          for (var j = 0; j < guest.preferences.length; j++) {
-            var preference = guest.preferences[j];
-
-            if (counter === 881) {
-              const paraPreference = document.createElement("p");
-              paraPreference.setAttribute("id", "preference" + j);
-              paraPreference.innerHTML = preference;
-
-              document
-                .getElementById("guestInfo" + i)
-                .appendChild(paraPreference);
+              textOverlay.appendChild(divName);
             }
-            console.log(preference);
+
+            console.log(guestName);
+            //for each guest write down the preferance
+            for (var j = 0; j < guest.preferences.length; j++) {
+              var preference = guest.preferences[j];
+
+              if (frame === 881) {
+                const paraPreference = document.createElement("p");
+                paraPreference.setAttribute("id", "preference" + j);
+                paraPreference.innerHTML = preference;
+
+                document
+                  .getElementById("guestInfo" + i)
+                  .appendChild(paraPreference);
+              }
+              console.log(preference);
+            }
           }
+        } else if (frame > 1690 && frame < 1880) {
+          textOverlay.innerHTML =
+            "<p id=insertedText>" +
+            "We are excited to see you <br> at our table " +
+            dataGuest.reservations[reservationId].guests[0].firstName +
+            "!" +
+            "</p>";
+        } else {
+          textOverlay.innerHTML = "";
         }
-      } else if (counter > 1690 && counter < 1880) {
-        textOverlay.innerHTML =
-          "<p id=insertedText>" +
-          "We are excited to see you <br> at our table " +
-          dataGuest.reservations[reservationId].guests[0].firstName +
-          "!" +
-          "</p>";
-      } else {
-        textOverlay.innerHTML = "";
-      }
-    }
+      },
+    });
 
     //Start the video when clicked
     document.getElementById("overlay").addEventListener("click", function (e) {
       //Play video
-      myVideo.play();
-
-      //Start timer
-      timedCount();
+      video.video.play();
+      video.listen("frame");
 
       //Remove play button
       overlay.classList.add("hide");
@@ -85,10 +85,8 @@ fetch("reservations.json")
     //Pause the video when clicked
     textOverlay.addEventListener("click", function (e) {
       //Pause video
-      myVideo.pause();
-
-      //Pause Timer
-      clearTimeout(timeout);
+      video.video.pause();
+      video.stopListen();
 
       //add play button
       overlay.classList.remove("hide");
@@ -96,8 +94,7 @@ fetch("reservations.json")
 
     //Reset timer after video ends
     function myHandeler(e) {
-      clearTimeout(timeout);
-      counter = 0;
+      video.stopListen();
 
       //Add play button
       overlay.classList.remove("hide");
