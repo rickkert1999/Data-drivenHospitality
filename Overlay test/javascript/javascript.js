d7bb2cd0c4a4888aaac8d9af815c8287b247df47
rickkert1999 <rickkert1999@gmail.com>
2022-04-14 14:02:55 +0200

    Fix duplicate text in video

--- Overlay test/javascript/javascript.js
+++ Overlay test/javascript/javascript.js
@@ -20,34 +20,34 @@ fetch("reservations.json")
         currentFrame.innerHTML = frame;
         console.log(frame);
 
-        if (frame > 400 && frame < 600) {
+        if (frame > 270 && frame < 415) {
           textOverlay.innerHTML =
             "<p id=insertedText>We are prepairing everything <br> for your visit!</p>";
-        } else if (frame > 880 && frame < 1108) {
-          //for each guest write down the name
-          for (
-            var i = 0;
-            i < dataGuest.reservations[reservationId].guests.length;
-            i++
-          ) {
-            var guest = dataGuest.reservations[reservationId].guests[i];
-            var guestName = guest.firstName + " " + guest.lastName;
+        } else if (frame > 590 && frame < 762) {
+          if (frame === 591) {
+            textOverlay.innerHTML = "";
 
-            if (frame === 881) {
+            //for each guest write down the name
+            for (
+              var i = 0;
+              i < dataGuest.reservations[reservationId].guests.length;
+              i++
+            ) {
+              var guest = dataGuest.reservations[reservationId].guests[i];
+              var guestName = guest.firstName + " " + guest.lastName;
+
               const divName = document.createElement("div");
               divName.setAttribute("id", "guestInfo" + i);
               divName.setAttribute("class", "guestInfo");
               divName.innerHTML = "<p id=name" + i + ">" + guestName + "</p>";
 
               textOverlay.appendChild(divName);
-            }
 
-            console.log(guestName);
-            //for each guest write down the preferance
-            for (var j = 0; j < guest.preferences.length; j++) {
-              var preference = guest.preferences[j];
+              console.log(guestName);
+              //for each guest write down the preferance
+              for (var j = 0; j < guest.preferences.length; j++) {
+                var preference = guest.preferences[j];
 
-              if (frame === 881) {
                 const paraPreference = document.createElement("p");
                 paraPreference.setAttribute("id", "preference" + j);
                 paraPreference.innerHTML = preference;
@@ -59,7 +59,7 @@ fetch("reservations.json")
               console.log(preference);
             }
           }
-        } else if (frame > 1690 && frame < 1880) {
+        } else if (frame > 1148 && frame < 1274) {
           textOverlay.innerHTML =
             "<p id=insertedText>" +
             "We are excited to see you <br> at our table " +
@@ -73,7 +73,7 @@ fetch("reservations.json")
     });
 
     //Start the video when clicked
-    document.getElementById("overlay").addEventListener("click", function (e) {
+    document.getElementById("overlay").addEventListener("click", function () {
       //Play video
       video.video.play();
       video.listen("frame");
@@ -83,7 +83,7 @@ fetch("reservations.json")
     });
 
     //Pause the video when clicked
-    textOverlay.addEventListener("click", function (e) {
+    textOverlay.addEventListener("click", function () {
       //Pause video
       video.video.pause();
       video.stopListen();
